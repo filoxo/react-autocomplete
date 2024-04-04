8786f22b025e6b904453c9779f96433629be7e2a
Carlos Filoteo <c.filoteo12@gmail.com>
2024-04-03 23:08:19 -0600

    Fix deps

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,25 @@
 module.exports = {
   stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-webpack5-compiler-swc'
+  ],
+
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
-    check: true, // type-check stories during Storybook build
+    // type-check stories during Storybook build
+    check: true,
+
+    reactDocgen: 'react-docgen-typescript'
+  },
+
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+
+  docs: {
+    autodocs: true
   }
 };
